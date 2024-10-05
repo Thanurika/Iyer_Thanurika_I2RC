bc4234db2d350845b0ed5a2e6208446022664f0b
Thanurika <Thanurikaiyer@gmail.com>
2024-10-05 09:55:45 -0700

    Worked on DriveTrain.Java File
    I added more to the code and then looked at errors. Got until slide 12 for the DriveTrain setup project.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,13 +8,35 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 
 public class Drivetrain extends SubsystemBase {
   private final WPI_TalonSRX leftDriveTalon;
   private final WPI_TalonSRX rightDriveTalon;
 
   /** Creates a new ExampleSubsystem. */
-  public Drivetrain() {}
+  public Drivetrain() {
+    //Initialize with corresponding ports
+    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.leftDriveTalonPort);
+    rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.rightDriveTalonPort);
+
+    //Set Drive trains to NeutralMode.Coast,
+    leftDriveTalon.setNeutralMode();
+    rightDriveTalon.setNeutralMode();
+    
+    //TODO::set rightDriveTalon inverted 
+    rightDriveTalon.BlahBlahBlah(/*Set Inverted */);
+
+  }
+  public boolean setSpeed(double leftSpeed, double rightSpeed) {
+    // Assuming 'leftTalon' and 'rightTalon' are instances of TalonSRX
+    boolean setLeftOk = leftTalon.setSpeed(leftSpeed);
+    boolean setRightOk = rightTalon.setSpeed(rightSpeed);
+
+    return (setLeftOk && setRightOk);
+}
+
 
   @Override
   public void periodic() {
